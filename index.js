cbf265c26c0c9c241f4ad600b865caac59c42e0d
Pooja SAXENA <nrjrasaxena@gmail.com>
2020-05-13 08:43:17 +0200

    fixed minor emacs syntex issue

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ exports.version = '0.1';
 exports.group = 'Demo Function';
 
 const dns = require('dns');
-const crypto = require('./fpe');                                                                                                                                        const cipher = crypto({});                                                                                                                                               
+const crypto = require('./fpe');
+const cipher = crypto({});                                                                                                                                               
  
 const ipv4Regex = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gm;
 const cache = {};
